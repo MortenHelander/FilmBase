4a26d7d5118e404f83b90c0fb31e7b2bae5e80cc
Morten <morten.helander@hotmail.com>
2025-11-17 12:09:20 +0100

    Added collection of films

--- src/filmbase/app/FilmBase.java
+++ src/filmbase/app/FilmBase.java
@@ -1,12 +1,32 @@
 package filmbase.app;
 
+import filmbase.data.Film;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class FilmBase {
+    private Collection<Film> allFilms = new ArrayList<>();
 
     public FilmBase(){
 
     }
 
+    private void initFilms(){
+        allFilms.add(new Film("Interstellar", 2014));
+        allFilms.add(new Film("Inception", 2010));
+        allFilms.add(new Film("El Laberinto del Fauno", 2006));
+        allFilms.add(new Film("Lord of the Rings: The Fellowship of the Ring", 2001));
+        allFilms.add(new Film("Lord of the Rings: The Two Towers", 2002));
+        allFilms.add(new Film("Lord of the Rings: Return of the King", 2003));
+        allFilms.add(new Film("The Godfather", 1972));
+        allFilms.add(new Film("The Shawshank Redemption", 1994));
+        allFilms.add(new Film("Schindler's List", 1993));
+        allFilms.add(new Film("The Great Gatsby", 2013));
+    }
+
     public void start(){
         System.out.println("FilmBasen er startet");
+        initFilms();
     }
 }
--- src/filmbase/data/Film.java
+++ src/filmbase/data/Film.java
@@ -1,4 +1,27 @@
 package filmbase.data;
 
 public class Film {
+    private String title;
+    private int year;
+
+    public Film(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
